a1543a5ccdbad7069d0e882b76d627192b020cc1
Facetozvers <whatthefapmc@gmail.com>
2022-03-29 16:22:40 +0700

    answer section quiz

--- app/Http/Controllers/Api/V1/Course/SectionQuizController.php
+++ app/Http/Controllers/Api/V1/Course/SectionQuizController.php
@@ -12,9 +12,9 @@ use App\Models\SectionContentOrder;
 class SectionQuizController extends Controller
 {
     public function show($section_quiz_id){
-        $section_quiz = SectionQuiz::with('quiz.questions')->find($section_quiz_id);
+        $section_quiz = SectionQuiz::with('quiz.questions.answers')->find($section_quiz_id);
         if(!$section_quiz){                          //gunakan slug untuk mengidentifikasi model
-            $section_quiz = SectionQuiz::with('quiz.questions')->where('slug', $section_quiz_id)->firstOrFail();
+            $section_quiz = SectionQuiz::with('quiz.questions.answers')->where('slug', $section_quiz_id)->firstOrFail();
         }
 
         return response($section_quiz);
